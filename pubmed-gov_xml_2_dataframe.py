a1c1e297f314e9fdfa24d48ef544a92d68bfae04
Dan Wendling <wendlingd@icloud.com>
2018-08-18 19:34:30 -0400

    Adds Tableau dashboard items

--- pubmed-gov_xml_2_dataframe.py
+++ pubmed-gov_xml_2_dataframe.py
@@ -5,26 +5,31 @@ Created on Tue Jul 17 17:38:22 2018
 
 @author: Dan Wendling
 
-Last modified: 2018-08-11
+Last modified: 2018-08-18
 
 Purpose: Ingest selected XML nodes from pubmed.gov data sets, File > XML exports,
 into Python dataframes.
+Search strategy used in development: https://www.nlm.nih.gov/services/queries/health_literacy.html
+
+Example of Tableau result: https://public.tableau.com/profile/danwendling#!/vizhome/Healthliteracy-NIHgrants/Healthliteracy-NIHgrants
 
 
 ----------------
  NOTES/WARNINGS
 ----------------
 
-* Pubmed.gov data is hierarchical XML; this process removes hierarchy, arranging
-the data instead as you might see in a relational database, where fields
-with repeating entries (authors, grants, MeSH, publication types, etc.) 
-in their own dataframes. So, use PMID as your "primary key" for pubmedarticle,
-and use it as the "foreign key" in the other tables.
-* Some records will be left out! Nothing is captured for <PubmedBookArticle>, 
-for example. The below captures journal articles, <PubmedArticle>.
+* Exported pubmed.gov XML is hierarchical; this script removes the hierarchy, 
+arranging data instead as you might see in a relational database, where fields
+with repeating entries (author, grant, MeSH, publication type, etc.) 
+in their own dataframes; PMID is the "primary key" for pubmedarticle, but the
+"foreign key" for the other dataframes.
+* A small number of records will be left out! Nothing is captured for 
+<PubmedBookArticle>, for example. The below captures journal articles, 
+<PubmedArticle>.
 * Structured abstracts use the NLM structure labels (@NlmCategory); the less
-standardized labels from the original source articles are not included.
-* Some elements are simplified. More items could be added - this is not 
+standardized labels in the original source articles are not captured.
+* Some elements are simplified, such as geting only the pmc (free full text)
+designation from the ArticleId element. More items could be added - this is not 
 everything. Each section has a URL for more information.
 * MeSH is thrown in here as one column, in case you want to use natural
 language processing on TI-AB-MeSH, etc. The MeSH content ends with 
@@ -56,8 +61,16 @@ SCRIPT CONTENTS
 5. Process/Explore grant
 6. Process/Explore chemical
 7. Process/Explore pubtype
+8. Write out, for next work session, visualization, etc.
 
 Partly based on https://stackoverflow.com/questions/49439081/nested-xml-file-to-pandas-dataframe
+but limited to articles with NIH grant numbers,
+(NIH[gr] OR Research Support, N.I.H., Extramural[pt] OR Research Support, N.I.H., Intramural[pt]) AND ((health[ti] AND literacy[ti]) OR ("health literacy"[All Fields] OR "health literate"[All Fields] OR "medical literacy"[All Fields]) OR (functional[tw] AND health[tw] AND literacy[tw]) OR numeracy[All Fields] OR ((low literate[ti] OR low literacy[ti] OR literacy[ti] OR illiteracy[ti] OR literate[ti] OR illiterate[ti] OR "reading"[MeSH Terms] OR "comprehension"[MeSH Terms]) AND ("health promotion"[MeSH Major Topic] OR "health education"[MeSH Major Topic] OR "patient education as topic"[MeSH Major Topic] OR "communication barriers"[MeSH Major Topic] OR "communication"[MeSH Major Topic:noexp] OR "health knowledge, attitudes, practice"[MeSH Major Topic] OR "attitude to health"[MeSH Major Topic])) OR ("comprehension"[MeSH Major Topic] AND "educational status"[MeSH Major Topic]) OR (family[ti] AND literacy[ti]) OR (("drug labeling"[All Fields] OR "prescriptions"[MeSH Terms]) AND ("comprehension"[All Fields] OR "numeracy"[All Fields])) OR ((cancer[ti] OR diabetes[ti]) AND (literacy[ti] OR comprehension[ti])) OR "adult literacy"[All Fields] OR "limited literacy"[All Fields] OR "patient understanding"[ti] OR ("self care"[MeSH Major Topic] AND "perception"[MeSH Terms]) OR (("comprehension"[MeSH Terms] OR "comprehension"[All Fields]) AND "food labeling"[MeSH Terms]) OR (("comprehension"[MeSH Terms] OR "comprehension"[All Fields]) AND ("informed consent"[MeSH Terms] OR ("informed"[All Fields] AND "consent"[All Fields]) OR "informed consent"[All Fields])) OR (("comprehension"[MeSH Terms] OR "comprehension"[All Fields]) AND ("insurance, health"[MeSH Terms] OR ("insurance"[All Fields] AND "health"[All Fields]) OR "health insurance"[All Fields] OR ("insurance"[All Fields] AND "health"[All Fields]) OR "insurance, health"[All Fields])) AND English[la]) 
+
+To construct URLs that will work in Tableau, etc., add your current selection 
+(year, journal name, chemical, PMID, whatever) after ?term= and the field
+tag and use +AND+ to make the search work, testing at pubmed.gov:
+https://www.ncbi.nlm.nih.gov/pubmed/?term=%28NIH%5Bgr%5D+OR+Research+Support%2C+N.I.H.%2C+Extramural%5Bpt%5D+OR+Research+Support%2C+N.I.H.%2C+Intramural%5Bpt%5D%29+AND+%28%28health%5Bti%5D+AND+literacy%5Bti%5D%29+OR+%28%22health+literacy%22%5BAll+Fields%5D+OR+%22health+literate%22%5BAll+Fields%5D+OR+%22medical+literacy%22%5BAll+Fields%5D%29+OR+%28functional%5Btw%5D+AND+health%5Btw%5D+AND+literacy%5Btw%5D%29+OR+numeracy%5BAll+Fields%5D+OR+%28%28low+literate%5Bti%5D+OR+low+literacy%5Bti%5D+OR+literacy%5Bti%5D+OR+illiteracy%5Bti%5D+OR+literate%5Bti%5D+OR+illiterate%5Bti%5D+OR+%22reading%22%5BMeSH+Terms%5D+OR+%22comprehension%22%5BMeSH+Terms%5D%29+AND+%28%22health+promotion%22%5BMeSH+Major+Topic%5D+OR+%22health+education%22%5BMeSH+Major+Topic%5D+OR+%22patient+education+as+topic%22%5BMeSH+Major+Topic%5D+OR+%22communication+barriers%22%5BMeSH+Major+Topic%5D+OR+%22communication%22%5BMeSH+Major+Topic%3Anoexp%5D+OR+%22health+knowledge%2C+attitudes%2C+practice%22%5BMeSH+Major+Topic%5D+OR+%22attitude+to+health%22%5BMeSH+Major+Topic%5D%29%29+OR+%28%22comprehension%22%5BMeSH+Major+Topic%5D+AND+%22educational+status%22%5BMeSH+Major+Topic%5D%29+OR+%28family%5Bti%5D+AND+literacy%5Bti%5D%29+OR+%28%28%22drug+labeling%22%5BAll+Fields%5D+OR+%22prescriptions%22%5BMeSH+Terms%5D%29+AND+%28%22comprehension%22%5BAll+Fields%5D+OR+%22numeracy%22%5BAll+Fields%5D%29%29+OR+%28%28cancer%5Bti%5D+OR+diabetes%5Bti%5D%29+AND+%28literacy%5Bti%5D+OR+comprehension%5Bti%5D%29%29+OR+%22adult+literacy%22%5BAll+Fields%5D+OR+%22limited+literacy%22%5BAll+Fields%5D+OR+%22patient+understanding%22%5Bti%5D+OR+%28%22self+care%22%5BMeSH+Major+Topic%5D+AND+%22perception%22%5BMeSH+Terms%5D%29+OR+%28%28%22comprehension%22%5BMeSH+Terms%5D+OR+%22comprehension%22%5BAll+Fields%5D%29+AND+%22food+labeling%22%5BMeSH+Terms%5D%29+OR+%28%28%22comprehension%22%5BMeSH+Terms%5D+OR+%22comprehension%22%5BAll+Fields%5D%29+AND+%28%22informed+consent%22%5BMeSH+Terms%5D+OR+%28%22informed%22%5BAll+Fields%5D+AND+%22consent%22%5BAll+Fields%5D%29+OR+%22informed+consent%22%5BAll+Fields%5D%29%29+OR+%28%28%22comprehension%22%5BMeSH+Terms%5D+OR+%22comprehension%22%5BAll+Fields%5D%29+AND+%28%22insurance%2C+health%22%5BMeSH+Terms%5D+OR+%28%22insurance%22%5BAll+Fields%5D+AND+%22health%22%5BAll+Fields%5D%29+OR+%22health+insurance%22%5BAll+Fields%5D+OR+%28%22insurance%22%5BAll+Fields%5D+AND+%22health%22%5BAll+Fields%5D%29+OR+%22insurance%2C+health%22%5BAll+Fields%5D%29%29+AND+English%5Bla%5D%29
 """
 
 
@@ -68,12 +81,12 @@ Partly based on https://stackoverflow.com/questions/49439081/nested-xml-file-to-
 
 import lxml.etree as et
 import pandas as pd
-# import os
+import os
 
 # Set working directory
-# os.chdir('/Users/name/Projects/pubmed-gov_xml_2_python_dataframe')
+os.chdir('/Users/name/Projects/pubmed-gov_xml_2_python_dataframe')
 
-# localDir = 'pubmed-gov_xml_2_python_dataframe/'
+localDir = 'pubmed-gov_xml_2_python_dataframe/'
 
 '''
 For your file of database records, Go to pubmed.gov, run your strategy, 
@@ -81,7 +94,7 @@ and select Send to > File > Format: XML > Create File
 '''
 
 # Set XML and XSLT to your filenames
-doc = et.parse("xml/Opioids-Caregivers.xml")
+doc = et.parse("xml/pubmed_result.xml")
 xsl_pubmedarticle = et.parse("xslt/tbl_pubmedarticle.xsl")
 xsl_author = et.parse("xslt/tbl_author.xsl")
 xsl_grant = et.parse("xslt/tbl_grant.xsl")
@@ -260,6 +273,14 @@ qualifierNameCounts = qualifierNameCounts.rename(columns={'QualifierName': 'Arti
 qualifierNameCounts.head(n=20)
 print("The Qualifier names represented, Top 20 (max)\n{}".format(qualifierNameCounts))
 
+# For Tableau, create table of "major" descriptors appearing at least 10 times
+meshMajorTable = mesh.loc[mesh['MajorDNTopicYN'].str.contains('Y') == True]
+# From https://stackoverflow.com/questions/30485151/python-pandas-exclude-rows-below-a-certain-frequency-count
+meshMajorTable = meshMajorTable.groupby('DescriptorName').filter(lambda x: len(x) >= 10)
+meshMajorTable = meshMajorTable[['PMID', 'DescriptorName', 'MajorDNTopicYN']]
+
+# Below - meshMajorTable will be one of the worksheets added to Excel export file.
+
 
 #%%
 # ===================================
@@ -354,6 +375,7 @@ chemicalCounts = chemicalCounts.rename(columns={'NameOfSubstance': 'Article coun
 chemicalCounts.head(n=20)
 print("The chemicals represented, Top 20 (max)\n{}".format(chemicalCounts))
 
+
 #%%
 # ===================================
 # 7. Process/Explore pubtype
@@ -398,3 +420,36 @@ pubTypeCount = pubtype['PublicationType'].value_counts().reset_index()
 pubTypeCount = pubTypeCount.rename(columns={'Affiliation': 'grant count', 'index': 'grant affiliation'})
 pubTypeCount.head(n=20)
 print("The pubtypes represented, Top 20 (max)\n{}".format(pubTypeCount))
+
+
+#%%
+# =========================================================
+# 8. Write out, for next work session, visualization, etc.
+# =========================================================
+'''
+Example for Tableau Public
+'''
+
+'''
+Year is part of pubmedarticle; the below creates a table to feed a Year bar 
+chart. Using DateCreatedYear, the year the pubmed.gov record was created. This
+is because records are built incrementally and while all have the date the 
+record was created, some new records don't have the date the article was/will
+be published. DateCreatedYear is more inclusive.
+'''
+
+ArticleListTable = pubmedarticle[['DateCreatedYear', 'PMID', 'ArticleTitle']]
+
+# Row must include DateCreatedYear; reduce columns
+yearTable = pubmedarticle[pd.notnull(pubmedarticle['DateCreatedYear'])]
+yearTable = yearTable[[ 'DateCreatedYear', 'PMID']]
+
+writer = pd.ExcelWriter('DataSource.xlsx')
+ArticleListTable.to_excel(writer,'ArticleList', index=False)
+yearTable.to_excel(writer,'DateCreatedYear', index=False)
+meshMajorTable.to_excel(writer,'MeshMajor', index=False)
+writer.save()
+
+
+# Example of pulling in Excel data, for follow-on work sessions
+# pubmedarticle = pd.read_excel('pubmedarticle.xlsx')
